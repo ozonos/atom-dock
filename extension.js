a600d914b214b501761efff578d018fd37a6e5ae
Muhammad Iqbal CR <boku.wa.kyuu@gmail.com>
2014-04-06 23:54:54 +0800

    Icon size appears correctly now. Hide background on overview.

--- extension.js
+++ extension.js
@@ -1,3 +1,23 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+
+/* Numix/Ozon Project 2014
+ * 
+ * Extension's version: 0.1
+ * 
+ * 0.1 Changes:
+ *  - added NosDock to contain NosDash and handle intellihide behaviors
+ *  - added NosDash containing favorite and running apps list
+ *  - hide dock's background on overview, taken from nos-panel
+ *  - convert indentation to spaces, added emacs header line
+ * 
+ * TODO(s): 
+ *  - fix gdm log message "Source ID <integer> was not found when attempting to remove it"
+ *    when hovering and unhovering icon
+ *  - fix icon's tooltip appearance
+ *  - use different name for NosDash?
+ *  - check behavior on multiple monitor
+ * 
+ */
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
@@ -12,15 +32,25 @@ let old_dash;
 let nosDock;
 
 function init() {
-  old_dash = new GnomeDash.gnomeDash();
+    old_dash = new GnomeDash.gnomeDash();
+}
+
+function setDockTransparent() {
+    nosDock.dash.actor.get_first_child().add_style_class_name('nos-hide-background');
+}
+
+function unsetDockTransparent() {
+    nosDock.dash.actor.get_first_child().remove_style_class_name('nos-hide-background');
 }
 
 function enable() {
-  old_dash.hideDash();
-  nosDock = new NosDock.NosDock();
+    old_dash.hideDash();
+    nosDock = new NosDock.NosDock();
+    Main.overview.connect('showing', setDockTransparent);
+    Main.overview.connect('hiding', unsetDockTransparent);
 }
 
 function disable() {
-  old_dash.showDash();
-  nosDock.destroy();
+    old_dash.showDash();
+    nosDock.destroy();
 }
--- nosdock.js
+++ nosdock.js
@@ -1,3 +1,5 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+
 const Signals = imports.signals;
 const Lang = imports.lang;
 const St = imports.gi.St;
@@ -9,91 +11,92 @@ const Me = ExtensionUtils.getCurrentExtension();
 const NosDash = Me.imports.nosdash;
 
 const NosDock = new Lang.Class({
-	Name: 'NosDock',
+    Name: 'NosDock',
 
-	_init: function() {
-		this._monitor = Main.layoutManager.primaryMonitor;
+    _init: function() {
+        this.dash = new NosDash.NosDash();
+        this.forcedOverview = false;
 
-		this.dash = new NosDash.NosDash();
-		this.forcedOverview = false;
+        this.dash.showAppsButton.connect('notify::checked', Lang.bind(this, this._onShowAppsButtonToggled));
 
-		// this.dash.actor.width = 500;
-		this.dash.showAppsButton.connect('notify::checked', Lang.bind(this, this._onShowAppsButtonToggled));
+        this._box = new St.BoxLayout({ name: 'nosDockBox', reactive: true, track_hover:true,
+            style_class: 'box' });
+        this.actor = new St.Bin({ name: 'nosDockContainer',reactive: false,
+            style_class: 'container', x_align: St.Align.MIDDLE, child: this._box});
 
-		this._box = new St.BoxLayout({ name: 'nosDockBox', reactive: true, track_hover:true,
-			style_class: 'box' });
-		this.actor = new St.Bin({ name: 'nosDockContainer',reactive: false,
-			style_class: 'container', x_align: St.Align.MIDDLE, child: this._box});
+        this._realizeId = this.actor.connect('realize', Lang.bind(this, this._initialize));
 
-		// this.actor.height = 52;
-		// this.actor.width = 500;
+        this._box.add_actor(this.dash.actor);
 
-		this._realizeId = this.actor.connect("realize", Lang.bind(this, this._initialize));
+        // Reset position when icon size changed
+        this.dash.connect('icon-size-changed', Lang.bind(this, this._resetPosition));
 
-		this._box.add_actor(this.dash.actor);
-
-		this.dash.connect('icon-size-changed', Lang.bind(this, this._resetPosition));
-
-		Main.uiGroup.add_child(this.actor);
+        Main.uiGroup.add_child(this.actor);
         Main.layoutManager._trackActor(this._box, { trackFullscreen: true });
         Main.layoutManager._trackActor(this.dash._box, { affectsStruts: false });
-	},
+    },
 
-	_initialize: function() {
-		this._resetPosition();
-	},
+    _initialize: function() {
+        if (this._realizeId > 0){
+            this.actor.disconnect(this._realizeId);
+            this._realizeId = 0;
+        }
 
-	_resetPosition: function() {
-		this._monitor = Main.layoutManager.primaryMonitor;
+        // Set initial position
+        this._resetPosition();
+    },
 
-		this.actor.width = Math.round(this._monitor.width);
+    _resetPosition: function() {
+        // Get the monitor
+        this._monitor = Main.layoutManager.primaryMonitor;
+
+        this.actor.width = Math.round(this._monitor.width);
         this.actor.x = this._monitor.x;
         this.actor.x_align = St.Align.MIDDLE;
-		this.actor.y = this._monitor.height - this.actor.height;
-		global.log(this.actor.height + ", " + this.actor.width);
-        //this.dash._container.set_width(-1);
-	},
+        this.actor.y = this._monitor.height - this.actor.height;
+        this.dash._container.set_width(-1);
+    },
 
-	_onShowAppsButtonToggled: function() {
+    _onShowAppsButtonToggled: function() {
 
-		// Sync the status of the default appButtons. Only if the two statuses are
-		// different, that means the user interacted with the extension provided
-		// application button, cutomize the behaviour. Otherwise the shell has changed the
-		// status (due to the _syncShowAppsButtonToggled function below) and it
-		// has already performed the desired action.
+        // Sync the status of the default appButtons. Only if the two statuses are
+        // different, that means the user interacted with the extension provided
+        // application button, cutomize the behaviour. Otherwise the shell has changed the
+        // status (due to the _syncShowAppsButtonToggled function below) and it
+        // has already performed the desired action.
 
-		let selector = Main.overview.viewSelector;
+        let selector = Main.overview.viewSelector;
 
-		if(selector._showAppsButton.checked !== this.dash.showAppsButton.checked){
+        if(selector._showAppsButton.checked !== this.dash.showAppsButton.checked){
 
-			if(this.dash.showAppsButton.checked){
-				if (!Main.overview._shown) {
-					// force entering overview if needed
-					Main.overview.show();
-					this.forcedOverview = true;
-				}
-				selector._showAppsButton.checked = true;
-			} else {
-				if (this.forcedOverview) {
-					// force exiting overview if needed
-					Main.overview.hide();
-					this.forcedOverview = false;
-				}
-				selector._showAppsButton.checked = false;
-			}
-		}
+            if(this.dash.showAppsButton.checked){
+                if (!Main.overview._shown) {
+                    // force entering overview if needed
+                    Main.overview.show();
+                    this.forcedOverview = true;
+                }
+                selector._showAppsButton.checked = true;
+            } else {
+                if (this.forcedOverview) {
+                    // force exiting overview if needed
+                    Main.overview.hide();
+                    this.forcedOverview = false;
+                }
+                selector._showAppsButton.checked = false;
+            }
+        }
 
-		// whenever the button is unactivated even if not by the user still reset the
-		// forcedOverview flag
-		if( this.dash.showAppsButton.checked==false)
-			this.forcedOverview = false;
-	},
+        // whenever the button is unactivated even if not by the user still reset the
+        // forcedOverview flag
+        if( this.dash.showAppsButton.checked==false)
+            this.forcedOverview = false;
+    },
 
-	destroy: function() {
-		// TODO: destroy signal
-		this.dash.destroy();
+    destroy: function() {
+        // TODO: destroy signal
+        this.dash.destroy();
         this.actor.destroy();
-	}
+    }
 
 });
 
