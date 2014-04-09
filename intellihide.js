5f57c5b32035d1d1bffa64b630e83b26fd3c48ec
Kyuu CR <boku.wa.kyuu@gmail.com>
2014-04-09 15:51:24 +0800

    Code cleanup on intellihide.js *okay.jpg*

--- intellihide.js
+++ intellihide.js
@@ -11,15 +11,13 @@ const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 
 const handledWindowTypes = [
-  Meta.WindowType.NORMAL,
-  // Meta.WindowType.DESKTOP,    // skip nautilus dekstop window
-  // Meta.WindowType.DOCK,       // skip other docks
-  Meta.WindowType.DIALOG,
-  Meta.WindowType.MODAL_DIALOG,
-  Meta.WindowType.TOOLBAR,
-  Meta.WindowType.MENU,
-  Meta.WindowType.UTILITY,
-  Meta.WindowType.SPLASHSCREEN
+    Meta.WindowType.NORMAL,
+    Meta.WindowType.DIALOG,
+    Meta.WindowType.MODAL_DIALOG,
+    Meta.WindowType.TOOLBAR,
+    Meta.WindowType.MENU,
+    Meta.WindowType.UTILITY,
+    Meta.WindowType.SPLASHSCREEN
 ];
 
 /*
@@ -33,17 +31,12 @@ const handledWindowTypes = [
  * Target object has to contain a Clutter.ActorBox object named staticBox and 
  * emit a 'box-changed' signal when this changes.
  * 
-*/
+ */
 
-const intellihide = new Lang.Class({
+ const intellihide = new Lang.Class({
     Name: 'Intellihide',
 
     _init: function(show, hide, target) {
-
-        // Load settings
-        // this._settings = settings;
-        // this._bindSettingsChanges();
-
         this._signalHandler = new Convenience.GlobalSignalHandler();
         this._tracker = Shell.WindowTracker.get_default();
         this._focusApp = null;
@@ -121,8 +114,7 @@ const intellihide = new Lang.Class({
                 global.screen,
                 'monitors-changed',
                 Lang.bind(this, this._updateDockVisibility)
-            ]
-        );
+            ]);
 
         // initialize: call show forcing to initialize status variable
         this._show(true);
@@ -132,47 +124,24 @@ const intellihide = new Lang.Class({
     },
 
     destroy: function() {
-
         // Disconnect global signals
         this._signalHandler.disconnect();
 
-        if (this._windowChangedTimeout > 0)
+        if (this._windowChangedTimeout > 0) {
             Mainloop.source_remove(this._windowChangedTimeout); // Just to be sure
+        }
         this._windowChangedTimeout = 0;
     },
 
-    // _bindSettingsChanges: function() {
-
-    //     this._settings.connect('changed::intellihide', Lang.bind(this, function(){
-    //         this._updateDockVisibility();
-    //     }));
-
-    //     this._settings.connect('changed::intellihide-perapp', Lang.bind(this, function(){
-    //         this._updateDockVisibility();
-    //     }));
-
-    //     this._settings.connect('changed::dock-fixed', Lang.bind(this, function(){
-    //         if(this._settings.get_boolean('dock-fixed')) {
-    //             this.status = true; // Since the dock is now shown
-    //         } else {
-    //             // Wait that windows rearrange after struts change
-    //             Mainloop.idle_add(Lang.bind(this, function() {
-    //                 this._updateDockVisibility();
-    //                 return false;
-    //             }));
-    //         }
-    //     }));
-    // },
-
     _show: function(force) {
-        if (this.status !== true || force){
+        if (this.status !== true || force) {
             this.status = true;
             this.showFunction();
         }
     },
 
     _hide: function(force) {
-        if (this.status !== false || force){
+        if (this.status !== false || force) {
             this.status = false;
             this.hideFunction();
         }
@@ -191,84 +160,71 @@ const intellihide = new Lang.Class({
     },
 
     _grabOpBegin: function() {
+        let INTERVAL = 100; // A good compromise between reactivity and efficiency; to be tuned.
 
-        //if(this._settings.get_boolean('intellihide')){
-            let INTERVAL = 100; // A good compromise between reactivity and efficiency; to be tuned.
+        if (this._windowChangedTimeout > 0) {
+            Mainloop.source_remove(this._windowChangedTimeout); // Just to be sure
+        }
 
-            if (this._windowChangedTimeout > 0)
-                Mainloop.source_remove(this._windowChangedTimeout); // Just to be sure
-
-            this._windowChangedTimeout = Mainloop.timeout_add(INTERVAL,
-                Lang.bind(this, function(){
-                    this._updateDockVisibility();
-                    return true; // to make the loop continue
-                })
-            );
-        //}
+        this._windowChangedTimeout = Mainloop.timeout_add(INTERVAL,
+            Lang.bind(this, function() {
+                this._updateDockVisibility();
+                return true; // to make the loop continue
+            })
+        );
     },
 
     _grabOpEnd: function() {
+        if (this._windowChangedTimeout > 0) {
+            Mainloop.source_remove(this._windowChangedTimeout);
+        }
 
-        //if(this._settings.get_boolean('intellihide')){
-            if (this._windowChangedTimeout > 0)
-                Mainloop.source_remove(this._windowChangedTimeout);
-
-            this._windowChangedTimeout = 0;
-            this._updateDockVisibility();
-        //}
+        this._windowChangedTimeout = 0;
+        this._updateDockVisibility();
     },
 
     _switchWorkspace: function(shellwm, from, to, direction) {
-        
         this._updateDockVisibility();
-
     },
 
     _updateDockVisibility: function() {
+        if (!this._disableIntellihide) {
+            let overlaps = false;
+            let windows = global.get_window_actors();
 
-        if( !(/*this._settings.get_boolean('dock-fixed') ||*/ this._disableIntellihide)) {
+            if (windows.length > 0) {
 
-            // if( this._settings.get_boolean('intellihide') ){
+                // This is the window on top of all others in the current workspace
+                let topWindow = windows[windows.length-1].get_meta_window();
+                // If there isn't a focused app, use that of the window on top
+                this._focusApp = this._tracker.focus_app || this._tracker.get_window_app(topWindow);
 
-                let overlaps = false;
-                let windows = global.get_window_actors();
+                windows = windows.filter(this._intellihideFilterInteresting, this);
 
-                if (windows.length > 0) {
+                for (let i = 0; i< windows.length; i++) {
 
-                    // This is the window on top of all others in the current workspace
-                    let topWindow = windows[windows.length-1].get_meta_window();
-                    // If there isn't a focused app, use that of the window on top
-                    this._focusApp = this._tracker.focus_app || this._tracker.get_window_app(topWindow);
+                    let win = windows[i].get_meta_window();
+                    if(win){
+                        let rect = win.get_outer_rect();
 
-                    windows = windows.filter(this._intellihideFilterInteresting, this);
+                        let test = ( rect.x < this._target.staticBox.x2) &&
+                        ( rect.x +rect.width > this._target.staticBox.x1 ) &&
+                        ( rect.y < this._target.staticBox.y2 ) &&
+                        ( rect.y +rect.height > this._target.staticBox.y1 );
 
-                    for (let i = 0; i< windows.length; i++) {
-
-                        let win = windows[i].get_meta_window();
-                        if(win){
-                            let rect = win.get_outer_rect();
-
-                            let test = ( rect.x < this._target.staticBox.x2) &&
-                                       ( rect.x +rect.width > this._target.staticBox.x1 ) &&
-                                       ( rect.y < this._target.staticBox.y2 ) &&
-                                       ( rect.y +rect.height > this._target.staticBox.y1 );
-
-                            if (test){
-                                overlaps = true;
-                                break;
-                            }
+                        if (test){
+                            overlaps = true;
+                            break;
                         }
                     }
                 }
+            }
 
-                if (overlaps) {
-                    this._hide();
-                } else {
-                    this._show();
-                }
-            // } else {
-            //     this._hide();
-            // }
+            if (overlaps) {
+                this._hide();
+            } else {
+                this._show();
+            }
         }
     },
 
@@ -276,7 +232,6 @@ const intellihide = new Lang.Class({
     // Consider all windows visible on the current workspace.
     // Optionally skip windows of other applications
     _intellihideFilterInteresting: function(wa){
-
         var currentWorkspace = global.screen.get_active_workspace_index();
 
         var meta_win = wa.get_meta_window();
@@ -291,7 +246,7 @@ const intellihide = new Lang.Class({
         var wksp_index = wksp.index();
 
         // Skip windows of other apps
-        // if(this._focusApp && this._settings.get_boolean('intellihide-perapp')) {
+        if (this._focusApp) {
             // The DropDownTerminal extension is not an application per se
             // so we match its window by wm class instead
             if (meta_win.get_wm_class() == 'DropDownTerminalWindow') {
@@ -305,7 +260,7 @@ const intellihide = new Lang.Class({
             if (this._focusApp != currentApp && !(meta_win.maximized_vertically && !meta_win.maximized_horizontally)) {
                 return false;
             }
-        // }
+        }
 
         if (wksp_index == currentWorkspace && meta_win.showing_on_its_workspace()) {
             return true;
@@ -320,8 +275,9 @@ const intellihide = new Lang.Class({
     _handledWindow: function(metaWindow) {
         // The DropDownTerminal extension uses the POPUP_MENU window type hint
         // so we match its window by wm class instead
-        if (metaWindow.get_wm_class() == 'DropDownTerminalWindow')
+        if (metaWindow.get_wm_class() == 'DropDownTerminalWindow') {
             return true;
+        }
 
         var wtype = metaWindow.get_window_type();
         for (var i = 0; i < handledWindowTypes.length; i++) {
